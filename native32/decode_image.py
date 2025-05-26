a57a833092494153118fb78d6cc764b7907792aa
gatecat <gatecat@ds0.me>
2025-05-26 10:07:18 +0200

    working on YUV
    Signed-off-by: gatecat <gatecat@ds0.me>

--- native32/decode_image.py
+++ native32/decode_image.py
@@ -4,35 +4,56 @@ import struct
 def decode_image_yuv(data, yuv_dump=None):
     width, height, img_size = struct.unpack("<HHL", data[0:8])
     out = bytearray(width * height * 4)
-    if yuv_dump is not None:
-        yuv = bytearray(width * height * 3)
-    i = 8
-    def _putxy(x, y, l, cr, cb):
-        if yuv_dump is not None:
-            yuv[(y * width + x) * 3 + 0] = l
-            yuv[(y * width + x) * 3 + 1] = cr
-            yuv[(y * width + x) * 3 + 2] = cb
+    y_2_2 = bytearray(width * height)
+    u_1_1 = bytearray((width // 2) * (height // 2))
+    v_1_1 = bytearray((width // 2) * (height // 2))
 
-        if cr == 0 and cb == 0:
-            out[(y * width + x) * 4 + 3] = 0
-            out[(y * width + x) * 4 + 2] = 0
-            out[(y * width + x) * 4 + 1] = 0
-            out[(y * width + x) * 4 + 0] = 0
-        else:
-            cr -= 128
-            cb -= 128
-            out[(y * width + x) * 4 + 2] = min(max(l + (45 * cr) // 32, 0), 255)
-            out[(y * width + x) * 4 + 1] = min(max(l - (11 * cb + 23 * cr) // 32, 0), 255)
-            out[(y * width + x) * 4 + 0] = min(max(l + (113 * cb) // 64, 0), 255)
-            out[(y * width + x) * 4 + 3] = 255
+    i = 8
 
     def _putquad(pix, data):
         y = (pix // (width // 2))
         x = (pix % (width // 2))
-        _putxy(2*x, 2*y, data[0], data[4], data[5])
-        _putxy(2*x+1, 2*y, data[2], data[4], data[5])
-        _putxy(2*x, 2*y+1, data[1], data[4], data[5])
-        _putxy(2*x+1, 2*y+1, data[3], data[4], data[5])
+        # Y
+        y_2_2[(2*y)*width + (2*x)] = data[0]
+        y_2_2[(2*y+1)*width + (2*x)] = data[1]
+        y_2_2[(2*y)*width + (2*x+1)] = data[2]
+        y_2_2[(2*y+1)*width + (2*x+1)] = data[3]
+        # U
+        u_1_1[pix] = data[5]
+        v_1_1[pix] = data[4]
+
+    def _clip(v):
+        return max(min(v, 255), 0)
+
+        # Interpolation: https://learn.microsoft.com/en-us/windows/win32/medfound/recommended-8-bit-yuv-formats-for-video-rendering#converting-420-yuv-to-422-yuv
+    def _interpolate_y(data, w, h):
+        h1 = h * 2
+        result = bytearray(w * h1)
+        for y in range(h):
+            for dy in range(2):
+                y1 = y * 2 + dy
+                for x in range(w):
+                    if dy == 0:
+                        result[y1 * w + x] = data[y * w + x] if y == 0 or data[y * w + x] != 0 else data[(y - 1) * w + x]
+                    else:
+                        result[y1 * w + x] = data[y * w + x] if y == (h-1) or data[y * w + x] != 0 else data[(y + 1) * w + x]
+
+        return result
+
+
+    def _interpolate_x(data, w, h):
+        w1 = w * 2
+        result = bytearray(w1 * h)
+        for y in range(h):
+            for x in range(w):
+                for dx in range(2):
+                    x1 = x * 2 + dx
+                    if dx == 0:
+                        result[y * w1 + x1] = data[y * w + x] if x == 0 or data[y * w + x] != 0 else data[y * w + (x-1)]
+                    else:
+                        result[y * w1 + x1] = data[y * w + x] if x == (w - 1) or data[y * w + x] != 0 else data[y * w + (x+1)]
+
+        return result
 
     pixel = 0
     while i < img_size+8 and pixel < ((width//2) * (height//2)):
@@ -52,8 +73,37 @@ def decode_image_yuv(data, yuv_dump=None):
                 _putquad(pixel, data[i:i+6])
                 pixel += 1
             i += 6
+
+
+
+
+    # yuv = bytearray(width * height * 3)
+
+    u_2_2 = _interpolate_x(_interpolate_y(u_1_1, width // 2, height // 2), width // 2, height)
+    v_2_2 = _interpolate_x(_interpolate_y(v_1_1, width // 2, height // 2), width // 2, height)
+
     if yuv_dump is not None:
-        yuv_dump.write(yuv)
+        yuv_dump.write(y_2_2)
+        yuv_dump.write(u_2_2)
+        yuv_dump.write(v_2_2)
+
+
+    for i in range(width * height):
+        if y_2_2[i] == 0: # transparent
+            out[i * 4 + 3] = 0
+            out[i * 4 + 2] = 0
+            out[i * 4 + 1] = 0
+            out[i * 4 + 0] = 0
+        else:
+            # YUV to RGB: https://learn.microsoft.com/en-us/windows/win32/medfound/recommended-8-bit-yuv-formats-for-video-rendering#converting-8-bit-yuv-to-rgb888
+            C = y_2_2[i] - 16
+            D = u_2_2[i] - 128
+            E = v_2_2[i] - 128
+            out[i * 4 + 3] = 255
+            out[i * 4 + 2] = _clip((298 * C           + 409 * E + 128) >> 8)
+            out[i * 4 + 1] = _clip((298 * C - 100 * D - 208 * E + 128) >> 8)
+            out[i * 4 + 0] = _clip((298 * C + 516 * D           + 128) >> 8)
+
     # assert pixel == (width//2) * (height//2)
     return image.frombytes(bytes(out), (width, height), "RGBA")
 
